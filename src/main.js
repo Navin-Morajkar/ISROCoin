d5314d41cf8a3ad2c5c9f64873c9f706ebf62f6b
Navin-Morajkar <91798534+Navin-Morajkar@users.noreply.github.com>
2022-08-28 23:51:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const ec = new EC('secp256k1');
 
 // Your private key goes here
 const myKey = ec.keyFromPrivate(
-  '7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c3e07c23e3273995cf'
+  '7c4c45907dec40c91bab3480c'
 );
 
 // From that we can calculate your public key (which doubles as your wallet address)
